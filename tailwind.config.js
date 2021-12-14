86a1e4ce248ce8a62d1f3b9ca59ba59120b02760
Askar <askar_1996@list.ru>
2021-12-14 20:34:56 +0600

    Первый блок;

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   content: ["*.html"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    fontFamily: {
+      body: ["Open Sans"],
+    },
+    screens: {
+      sm: "540px",
+      md: "720px",
+      lg: "960px",
+      xl: "1250px",
+    },
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url('../img/home.jpg')",
+      },
+    },
   },
   plugins: [],
 };
